303786ea8db0e9e3d455d8619390283c26f40905
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-01-27 19:37:42 -0300

    feat: mapeamento entidade Agendamento

---
+++ ClinicasCRM.Api/Data/Mappings/Agendamento/AgendamentoMapping.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClinicasCRM.Api.Data.Mappings.Agendamento;
+
+public class AgendamentoMapping : IEntityTypeConfiguration<ClinicasCRM.Core.Models.Agendamento.Agendamento>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<ClinicasCRM.Core.Models.Agendamento.Agendamento> builder)
+    {
+        builder.ToTable("Agendamentos");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.SalaAtendimento)
+            .IsRequired()
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.DiaAtendimento)
+            .IsRequired()
+            .HasColumnType("TIMESTAMP");
+
+        builder.Property(x => x.Profissional)
+            .IsRequired()
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.Duracao)
+            .IsRequired()
+            .HasColumnType("INTEGER");
+
+        builder.Property(x => x.StatusAtendimento)
+            .IsRequired()
+            .HasColumnType("SMALLINT");
+    }
+}
