e6665a426e83b28f10707864ae7010bc6f2502ba
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-01-27 19:36:42 -0300

    feat: adicionado AppDbContext

---
+++ ClinicasCRM.Api/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using ClinicasCRM.Core.Models.Agendamento;
+using ClinicasCRM.Core.Models.Avaliacao;
+using ClinicasCRM.Core.Models.Consulta;
+using ClinicasCRM.Core.Models.Paciente;
+using ClinicasCRM.Core.Models.Pessoa;
+using ClinicasCRM.Core.Models.Procedimento;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ClinicasCRM.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<PessoaFisica> PessoasFisica { get; set; }
+    public DbSet<PessoaJuridica> PessoasJuridica { get; set; }
+    public DbSet<Procedimento> Procedimentos { get; set; }
+    public DbSet<Habitos> Habitos { get; set; }
+    public DbSet<HabitosFemininos> HabitosFemininos { get; set; }
+    public DbSet<Medidas> Medidas { get; set; }
+    public DbSet<Anamnese> Anamneses { get; set; }
+    public DbSet<AnamneseCorporal> AnamnesesCorporal { get; set; }
+    public DbSet<AnamneseFacial> AnamnesesFacial { get; set; }
+    public DbSet<AvaliacaoCorporal> AvaliacoesCorporal { get; set; }
+    public DbSet<AvaliacaoFacial> AvaliacoesFacial { get; set; }
+    public DbSet<Agendamento> Agendamentos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
