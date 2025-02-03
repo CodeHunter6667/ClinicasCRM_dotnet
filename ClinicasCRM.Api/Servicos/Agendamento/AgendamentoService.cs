4d672c48d234d0c6ce25a0ab184a00d8f070e97e
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-02-03 20:24:35 -0300

    feat: criado serviço e repositório para Agendamento

---
+++ ClinicasCRM.Api/Servicos/Agendamento/AgendamentoService.cs
@@ -0,0 +1,9 @@
+﻿using ClinicasCRM.Api.Repositorios.Agendamento;
+using ClinicasCRM.Api.Servicos.Agendamento.Interfaces;
+using ClinicasCRM.Api.Servicos.Base;
+
+namespace ClinicasCRM.Api.Servicos.Agendamento;
+
+public class AgendamentoService : ServicoBase<ClinicasCRM.Core.Models.Agendamento.Agendamento, AgendamentoRepositorio>, IAgendamentoService
+{
+}
---
+++ ClinicasCRM.Api/Servicos/Agendamento/Interfaces/IAgendamentoService.cs
@@ -0,0 +1,7 @@
+﻿using ClinicasCRM.Api.Servicos.Base.Interfaces;
+
+namespace ClinicasCRM.Api.Servicos.Agendamento.Interfaces;
+
+public interface IAgendamentoService : IServicoBase<ClinicasCRM.Core.Models.Agendamento.Agendamento>
+{
+}
