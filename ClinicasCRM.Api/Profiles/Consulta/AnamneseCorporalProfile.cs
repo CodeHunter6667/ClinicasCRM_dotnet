b770f0c325a0f4258580eda1b6958fcc6f30adf3
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-02-08 17:42:20 -0300

    feat: criado Dto e Profile das Anamneses

---
+++ ClinicasCRM.Api/Profiles/Consulta/AnamneseCorporalProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ClinicasCRM.Core.Dtos.Consulta;
+using ClinicasCRM.Core.Models.Consulta;
+
+namespace ClinicasCRM.Api.Profiles.Consulta;
+
+public class AnamneseCorporalProfile : Profile
+{
+    public AnamneseCorporalProfile()
+    {
+        CreateMap<AnamneseCorporal, AnamneseCorporalDto>().ReverseMap();
+    }
+}
---
+++ ClinicasCRM.Core/Dtos/Consulta/AnamneseCorporalDto.cs
@@ -0,0 +1,32 @@
+﻿using ClinicasCRM.Core.Models.Consulta;
+using System.Text.Json.Serialization;
+
+namespace ClinicasCRM.Core.Dtos.Consulta;
+
+public class AnamneseCorporalDto
+{
+    public long Id { get; set; }
+    public string PrincipaisQueixas { get; set; } = string.Empty;
+    public long MedidasId { get; set; }
+    public string AnotacoesTratamentoEscolhido { get; set; } = string.Empty;
+    public DateTime DataAvaliacao { get; set; } = DateTime.Now;
+    public string Observacoes { get; set; } = string.Empty;
+    public long ClienteId { get; set; }
+
+    public AnamneseCorporalDto(AnamneseCorporal anamnese)
+    {
+        Id = anamnese.Id;
+        PrincipaisQueixas = anamnese.PrincipaisQueixas;
+        MedidasId = anamnese.MedidasId;
+        AnotacoesTratamentoEscolhido = anamnese.AnotacoesTratamentoEscolhido;
+        DataAvaliacao = anamnese.DataAvaliacao;
+        Observacoes = anamnese.Observacoes;
+        ClienteId = anamnese.ClienteId;
+    }
+
+    [JsonConstructor]
+    public AnamneseCorporalDto()
+    {
+        
+    }
+}
