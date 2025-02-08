23befb57509aacc41bc6d2ea5aa5edee5864a436
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-02-08 17:44:46 -0300

    feat: criado Dto e Profile de Procedimento

---
+++ ClinicasCRM.Api/Profiles/Procedimento/ProcedimentoProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using ClinicasCRM.Core.Dtos.Procedimento;
+
+namespace ClinicasCRM.Api.Profiles.Procedimento;
+
+public class ProcedimentoProfile : Profile
+{
+    public ProcedimentoProfile()
+    {
+        CreateMap<ClinicasCRM.Core.Models.Procedimento.Procedimento, ProcedimentoDto>().ReverseMap();
+    }
+}
---
+++ ClinicasCRM.Core/Dtos/Procedimento/ProcedimentoDto.cs
@@ -0,0 +1,24 @@
+﻿using ClinicasCRM.Core.Enums;
+
+namespace ClinicasCRM.Core.Dtos.Procedimento;
+public class ProcedimentoDto
+{
+    public long Id { get; set; }
+    public string NomeProcedimento { get; set; } = string.Empty;
+    public int Duracao { get; set; }
+    public string EquipamentosUtilizados { get; set; } = string.Empty;
+    public string ConsumoProdutos { get; set; } = string.Empty;
+    public decimal Preco { get; set; }
+    public EFormaPagamento FormaPagamento { get; set; }
+
+    public ProcedimentoDto(Models.Procedimento.Procedimento procedimento)
+    {
+        Id = procedimento.Id;
+        NomeProcedimento = procedimento.NomeProcedimento;
+        Duracao = procedimento.Duracao;
+        EquipamentosUtilizados = procedimento.EquipamentosUtilizados;
+        ConsumoProdutos = procedimento.ConsumoProdutos;
+        Preco = procedimento.Preco;
+        FormaPagamento = procedimento.FormaPagamento;
+    }
+}
