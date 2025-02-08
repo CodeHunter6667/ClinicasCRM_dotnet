abee050fd040caea6d76cd30a511087a29a1e13e
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-02-08 17:41:35 -0300

    feat: criado Dto e Profile de Agendamento

---
+++ ClinicasCRM.Api/Profiles/Agendamento/AgendamentoProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using ClinicasCRM.Core.Dtos.Agendamento;
+
+namespace ClinicasCRM.Api.Profiles.Agendamento;
+
+public class AgendamentoProfile : Profile
+{
+    public AgendamentoProfile()
+    {
+        CreateMap<Core.Models.Agendamento.Agendamento, AgendamentoDto>().ReverseMap();
+    }
+}
---
+++ ClinicasCRM.Core/Dtos/Agendamento/AgendamentoDto.cs
@@ -0,0 +1,34 @@
+﻿using ClinicasCRM.Core.Enums;
+using System.Text.Json.Serialization;
+
+namespace ClinicasCRM.Core.Dtos.Agendamento;
+
+public class AgendamentoDto
+{
+    public long Id { get; set; }
+    public long ClienteId { get; set; }
+    public string SalaAtendimento { get; set; } = string.Empty;
+    public long ProcedimentoId { get; set; }
+    public int Duracao { get; set; }
+    public DateTime DiaAtendimento { get; set; }
+    public string Profissional { get; set; } = string.Empty;
+    public EStatusAtendimento StatusAtendimento { get; set; }
+
+    public AgendamentoDto(Models.Agendamento.Agendamento agendamento)
+    {
+        Id = agendamento.Id;
+        ClienteId = agendamento.ClienteId;
+        SalaAtendimento = agendamento.SalaAtendimento;
+        ProcedimentoId = agendamento.ProcedimentoId;
+        Duracao = agendamento.Duracao;
+        DiaAtendimento = agendamento.DiaAtendimento;
+        Profissional = agendamento.Profissional;
+        StatusAtendimento = agendamento.StatusAtendimento;
+    }
+
+    [JsonConstructor]
+    public AgendamentoDto()
+    {
+        
+    }
+}
