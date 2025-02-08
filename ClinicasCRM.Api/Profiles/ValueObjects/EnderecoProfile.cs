bc123039a24a1bb2a7e365de50866283c4b91a6c
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-02-08 17:45:27 -0300

    feat: criado Dto e Profile de Endereço

---
+++ ClinicasCRM.Api/Profiles/ValueObjects/EnderecoProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ClinicasCRM.Core.Dtos.ValueObjects;
+using ClinicasCRM.Core.ValueObjects;
+
+namespace ClinicasCRM.Api.Profiles.ValueObjects;
+
+public class EnderecoProfile : Profile
+{
+    public EnderecoProfile()
+    {
+        CreateMap<Endereco, EnderecoDto>().ReverseMap();
+    }
+}
---
+++ ClinicasCRM.Core/Dtos/ValueObjects/EnderecoDto.cs
@@ -0,0 +1,33 @@
+﻿using ClinicasCRM.Core.ValueObjects;
+using System.Text.Json.Serialization;
+
+namespace ClinicasCRM.Core.Dtos.ValueObjects;
+public class EnderecoDto
+{
+    public long Id { get; set; }
+    public string Logradouro { get; set; } = string.Empty;
+    public string Numero { get; set; } = string.Empty;
+    public string Bairro { get; set; } = string.Empty;
+    public string Complemento { get; set; } = string.Empty;
+    public string Cidade { get; set; } = string.Empty;
+    public string Uf { get; set; } = string.Empty;
+    public string Cep { get; set; } = string.Empty;
+
+    public EnderecoDto(Endereco endereco)
+    {
+        Id = endereco.Id;
+        Logradouro = endereco.Logradouro;
+        Numero = endereco.Numero;
+        Bairro = endereco.Bairro;
+        Complemento = endereco.Complemento;
+        Cidade = endereco.Cidade;
+        Uf = endereco.Uf;
+        Cep = endereco.Cep;
+    }
+
+    [JsonConstructor]
+    public EnderecoDto()
+    {
+        
+    }
+}
