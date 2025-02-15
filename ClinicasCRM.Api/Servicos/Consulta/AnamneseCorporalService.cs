fd0bcc62fd81de0ac8a8c4fc57852ce151c62f9c
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-02-15 18:22:30 -0300

    feat: adicionado métodos para AnamneseCorporalService

--- ClinicasCRM.Api/Servicos/Consulta/AnamneseCorporalService.cs
+++ ClinicasCRM.Api/Servicos/Consulta/AnamneseCorporalService.cs
@@ -1,10 +1,29 @@
-﻿using ClinicasCRM.Api.Repositorios.Consulta;
+﻿using AutoMapper;
+using ClinicasCRM.Api.Repositorios.Consulta;
 using ClinicasCRM.Api.Servicos.Base;
 using ClinicasCRM.Api.Servicos.Consulta.Interfaces;
+using ClinicasCRM.Core.Dtos.Consulta;
+using ClinicasCRM.Core.Exceptions;
 using ClinicasCRM.Core.Models.Consulta;
 
 namespace ClinicasCRM.Api.Servicos.Consulta;
 
-public class AnamneseCorporalService : ServicoBase<AnamneseCorporal, AnamneseCorporalRepositorio>, IAnamneseCorporalService
+public class AnamneseCorporalService(IMapper _mapper) : ServicoBase<AnamneseCorporal, AnamneseCorporalRepositorio>, IAnamneseCorporalService
 {
+    
+    public async Task<AnamneseCorporalDto> ObterPorId(long id)
+    {
+        var anamnese = await ObterAsync(id);
+        if(anamnese is null)
+            throw new NotFoundException("Anamnese não encontrada");
+
+        return _mapper.Map<AnamneseCorporalDto>(anamnese);
+    }
+
+    public async Task<List<AnamneseCorporalDto>> TodosPorCliente(long clienteId)
+    {
+        var anamneses = await TodosAsync();
+        anamneses = anamneses.Where(a => a.ClienteId == clienteId).ToList();
+        return _mapper.Map<List<AnamneseCorporalDto>>(anamneses);
+    }
 }
--- ClinicasCRM.Api/Servicos/Consulta/Interfaces/IAnamneseCorporalService.cs
+++ ClinicasCRM.Api/Servicos/Consulta/Interfaces/IAnamneseCorporalService.cs
@@ -1,8 +1,11 @@
 ﻿using ClinicasCRM.Api.Servicos.Base.Interfaces;
+using ClinicasCRM.Core.Dtos.Consulta;
 using ClinicasCRM.Core.Models.Consulta;
 
 namespace ClinicasCRM.Api.Servicos.Consulta.Interfaces;
 
 public interface IAnamneseCorporalService : IServicoBase<AnamneseCorporal>
 {
+    Task<List<AnamneseCorporalDto>> TodosPorCliente(long clienteId);
+    Task<AnamneseCorporalDto> ObterPorId(long id);
 }
