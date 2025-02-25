4067a1e5a139fd0fd9b963893ab319e673902adb
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-02-24 21:11:51 -0300

    feat: criado endpoint para HabitosMasculinos

--- ClinicasCRM.Api/Servicos/Paciente/HabitosMasculinosService.cs
+++ ClinicasCRM.Api/Servicos/Paciente/HabitosMasculinosService.cs
@@ -1,14 +1,63 @@
-﻿using ClinicasCRM.Api.Data;
+﻿using AutoMapper;
+using ClinicasCRM.Api.Data;
 using ClinicasCRM.Api.Servicos.Base;
 using ClinicasCRM.Api.Servicos.Paciente.Interfaces;
+using ClinicasCRM.Core.Dtos.Paciente;
+using ClinicasCRM.Core.Exceptions;
 using ClinicasCRM.Core.Models.Paciente;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClinicasCRM.Api.Servicos.Paciente;
 
 public class HabitosMasculinosService : ServicoBase<HabitosMasculinos>, IHabitosMasculinosService
 {
-    public HabitosMasculinosService(AppDbContext context) : base(context)
+    private IMapper _mapper;
+
+    public HabitosMasculinosService(AppDbContext context, IMapper mapper) : base(context)
     {
-        
+        _mapper = mapper;
+    }
+
+    public async Task<HabitosMasculinosDto> AtualizarAsync(long id, HabitosMasculinosDto dto)
+    {
+        var habitos = await ObterAsync(id, dto.UsuarioId);
+        if (habitos is null)
+            throw new NotFoundException("Hábitos não encontrados");
+        _mapper.Map(dto, habitos);
+        habitos = await AtualizarAsync(habitos);
+        return _mapper.Map<HabitosMasculinosDto>(habitos);
+    }
+
+    public async Task<HabitosMasculinosDto> InserirAsync(HabitosMasculinosDto dto)
+    {
+        var habitos = new HabitosMasculinos();
+        _mapper.Map(dto, habitos);
+        habitos = await InserirAsync(habitos);
+        return _mapper.Map<HabitosMasculinosDto>(habitos);
+    }
+
+    public Task<List<HabitosMasculinosDto>> ObterPorClienteAsync(long clienteId, string usuarioId)
+    {
+        var habitos = _context.HabitosMasculinos
+        .AsNoTracking()
+        .Where(x => x.ClienteId == clienteId
+        && x.UsuarioId.Equals(usuarioId, StringComparison.InvariantCultureIgnoreCase));
+        return _mapper.ProjectTo<HabitosMasculinosDto>(habitos).ToListAsync();
+    }
+
+    public async Task<HabitosMasculinosDto> ObterPorIdAsync(long id, string usuarioId)
+    {
+        var habitos = await ObterAsync(id, usuarioId);
+        if (habitos is null)
+            throw new NotFoundException("Hábitos não encontrados");
+        return _mapper.Map<HabitosMasculinosDto>(habitos);
+    }
+
+    async Task<List<HabitosMasculinosDto>> IHabitosMasculinosService.TodosAsync(string usuarioId)
+    {
+        var habitos = _context.HabitosMasculinos
+        .AsNoTracking()
+        .Where(x => x.UsuarioId.Equals(usuarioId, StringComparison.InvariantCultureIgnoreCase));
+        return _mapper.Map<List<HabitosMasculinosDto>>(await habitos.ToListAsync());
     }
 }
--- ClinicasCRM.Api/Servicos/Paciente/Interfaces/IHabitosMasculinosService.cs
+++ ClinicasCRM.Api/Servicos/Paciente/Interfaces/IHabitosMasculinosService.cs
@@ -1,8 +1,15 @@
-﻿using ClinicasCRM.Api.Servicos.Base.Interfaces;
+﻿using System.Security.Claims;
+using ClinicasCRM.Api.Servicos.Base.Interfaces;
+using ClinicasCRM.Core.Dtos.Paciente;
 using ClinicasCRM.Core.Models.Paciente;
 
 namespace ClinicasCRM.Api.Servicos.Paciente.Interfaces;
 
 public interface IHabitosMasculinosService : IServicoBase<HabitosMasculinos>
 {
+    Task<List<HabitosMasculinosDto>> TodosAsync(string usuarioId);
+    Task<HabitosMasculinosDto> ObterPorIdAsync(long id, string usuarioId);
+    Task<HabitosMasculinosDto> AtualizarAsync(long id, HabitosMasculinosDto dto);
+    Task<HabitosMasculinosDto> InserirAsync(HabitosMasculinosDto dto);
+    Task<List<HabitosMasculinosDto>> ObterPorClienteAsync(long clienteId, string usuarioId);
 }
